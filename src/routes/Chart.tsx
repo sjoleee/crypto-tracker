3d867540a34040e2dac94d23c53a4f6323005082
sjoleee <cent7425@gmail.com>
2022-07-16 17:48:36 +0900

    chart 구현을 위한 가격변동 데이터를 react query 사용하여 fetch

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,4 +1,26 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+
+interface HistoryData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId?: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<HistoryData>(["history", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  console.log(data);
   return <div>chart</div>;
 }
 
